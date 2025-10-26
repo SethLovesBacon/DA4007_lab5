892c2f55c6ac99e09403b1c0908386c95ce3f0f5
SethLovesBacon <seth.gousmett@outlook.com>
2025-10-26 05:11:09 +0100

    Create main.cpp
    Added in main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: slm <int> <file> <int>" << endl;
+        return 1;
+    }
+
+}
