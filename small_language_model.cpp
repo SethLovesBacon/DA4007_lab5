1f1a3ec99114a0b8b95b0207349111f2352ccac5
SethLovesBacon <seth.gousmett@outlook.com>
2025-10-27 19:31:22 +0100

    updated SLM
    edited the train method and added two new methods to output the word freq and next char freq table

--- small_language_model.cpp
+++ small_language_model.cpp
@@ -8,13 +8,53 @@
 using namespace std; 
 
 
-Small_Language_Model::Small_Language_Model(string text, int word_size, int output_size) {
-    text = text;
-    word_size = word_size;
-    output_size = output_size;
+Small_Language_Model::Small_Language_Model(string input_text, int input_word_size, int input_output_size) {
+    text = input_text;
+    word_size = input_word_size;
+    output_size = input_output_size;
 }
 
 
 void Small_Language_Model::train() {
-    
+    word_count = 0;
+    for (int i = 0; i < text.size() - word_size + 1; i++) {
+        string sub_str = text.substr(i, word_size);
+
+        char next_char;
+        if (i == text.size() - word_size + 1) { next_char = ' '; }
+        else { next_char = text[i + 1]; }
+
+        if (word_freq.count(sub_str) == 0) { 
+            word_freq[sub_str] = 1; 
+            next_char_freq[sub_str][next_char] = 1;
+        }
+        else {
+            word_freq[sub_str] += 1;
+            if (next_char_freq[sub_str].count(next_char) == 0) {
+                next_char_freq[sub_str][next_char] = 1;
+            }
+            else { next_char_freq[sub_str][next_char] += 1; }
+            
+        }
+        word_count++;
+    }
+}
+
+
+void Small_Language_Model::get_word_freq() {
+    for (auto word : word_freq) {
+        cout << word.first << ": " << word.second << endl;
+    }
+}
+
+
+void Small_Language_Model::get_next_char_freq() {
+    for (auto word : next_char_freq) {
+        map<char, int>char_freq = word.second;
+
+        for (auto character : char_freq) {
+            cout << word.first << " " << character.first << " " << character.second << endl;
+        }
+    }
+
 }
--- small_language_model.h
+++ small_language_model.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <map>
 using namespace std;
 
 class Small_Language_Model {
@@ -9,9 +10,14 @@ class Small_Language_Model {
     Small_Language_Model(string, int, int);
 
     void train();
+    void get_word_freq();
+    void get_next_char_freq();
 
     private:
     string text;
     int word_size;
     int output_size;
+    int word_count;
+    map<string, int> word_freq;
+    map<string, map<char, int>> next_char_freq;
 };
